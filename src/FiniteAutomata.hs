c63f7a50aefdd626e40de5c99a803c43168ba274
Gustavo Bicalho <gusbicalho@gmail.com>
2019-05-28 16:06:30 +0000

    more fun with FiniteAutomata

--- src/FiniteAutomata.hs
+++ src/FiniteAutomata.hs
@@ -21,9 +21,31 @@ module FiniteAutomata where
   instance (Eq s) => ToTransition (s, [s]) s where
     toTransition (expect, dest) s = [d | d <- dest, s == expect]
 
-  -- examples below
+  type TransitionTable s i = [(s, i, s)]
+  transitions :: (Eq s, Eq i) => TransitionTable s i -> i -> Transition s
+  transitions table i s = [dest | (start, input, dest) <- table,
+                                  start == s, input == i]
+
+  process :: (Eq s, Eq i) => TransitionTable s i -> [s] -> [i] -> [s]
+  process table states inputs = foldl (>>=) states $ map tt inputs
+    where tt = transitions table
+
+  -- example below
 
   data State = Initial | Second | Third deriving (Eq, Show)
+  data Input = A | B | C deriving (Eq, Show)
+
+  table = [(Initial, A, Initial)
+          ,(Initial, B, Second)
+          ,(Initial, B, Third)
+          ,(Initial, C, Third)
+          ,(Second,  A, Second)
+          ,(Second,  B, Second)
+          ,(Second,  C, Initial)
+          ,(Second,  C, Third)
+          ,(Third,   C, Third)
+          ]
+  tt = transitions table
 
   a = foldl (-:-) dead [(Initial, [Initial])
                        ,(Second,  [Second])]
@@ -37,11 +59,12 @@ module FiniteAutomata where
                   ,Third  ])
     -:- (Third,   [Third  ])
 
-{--
->>> [Initial] >>= a >>= b >>= a >>= c >>= c
-[Third,Third]
-
->>> foldl (>>=) [Initial] [a, b, a, c, c]
-[Third,Third]
-
---}
+  tests = [ [Initial] >>= a >>= b >>= a >>= c >>= c
+          , foldl (>>=) [Initial] [a, b, a, c, c]
+          , [Initial] >>= tt A >>= tt B >>= tt A >>= tt C >>= tt C
+          , foldl (>>=) [Initial] $ map tt [A, B, A, C, C]
+          , process table [Initial] [A, B, A, C, C]
+          ]
+  -- >>> all ([Third,Third] ==) tests
+  -- True
+  --
