e0b533ae9a478d0d9a64160b972aa9c8c17bece7
Gustavo Bicalho <gusbicalho@gmail.com>
2019-05-30 20:38:47 +0000

    add postfix operator for end of htreelist

--- src/HTree.hs
+++ src/HTree.hs
@@ -1,6 +1,6 @@
 {-# LANGUAGE
     DataKinds, FlexibleContexts, FlexibleInstances, GADTs
-  , KindSignatures, PolyKinds, TypeOperators
+  , KindSignatures, PolyKinds, TypeOperators, PostfixOperators
 #-}
 
 module HTree where
@@ -10,8 +10,12 @@ module HTree where
 
   data HTreeList (ts :: [RoseTree *]) where
     HNil :: HTreeList '[]
-    (:^:) :: HTree t -> HTreeList ts -> HTreeList (t ': ts)
-  infixr 3 :^:
+    (:>:) :: HTree t -> HTreeList ts -> HTreeList (t ': ts)
+  infixr 5 :>:
+
+  (>.) :: HTree t -> HTreeList '[t]
+  (>.) t = t :>: HNil
+  infixl 3 >.
 
   data HTree (treeT :: RoseTree *) where
     HT :: a -> HTreeList ts -> HTree ('R a ts)
@@ -23,7 +27,7 @@ module HTree where
     HNil == HNil = True
 
   instance (Eq (HTree t), Eq (HTreeList ts)) => (Eq (HTreeList (t ': ts))) where
-    (t :^: ts) == (u :^: us) = t == u && ts == us
+    (t :>: ts) == (u :>: us) = t == u && ts == us
 
   instance (Eq a, Eq (HTreeList (x ': xs))) => Eq (HTree (R a (x ': xs))) where
     (HT a as) == (HT b bs) = a == b && as == bs
@@ -40,9 +44,13 @@ module HTree where
     showsPrec _ _ s = "HNil" ++ s
 
   instance (Show (HTree t), Show (HTreeList ts)) => (Show (HTreeList (t ': ts))) where
-    showsPrec d (t :^: ts) = showParen (d > upPrec) $
+    showsPrec d (t :>: HNil) = showParen (d > upPrec) $
         showsPrec (upPrec + 1) t .
-        showString " :^: " .
+        showString " >."
+      where upPrec = 3
+    showsPrec d (t :>: ts) = showParen (d > upPrec) $
+        showsPrec (upPrec + 1) t .
+        showString " :>: " .
         showsPrec (upPrec + 1) ts
       where upPrec = 3
 
@@ -54,27 +62,34 @@ module HTree where
       where appPrec = 10
 
 {-
+>>> :set -XPostfixOperators
 >>> newtype X = X Int deriving (Eq, Show)
 >>> newtype Y = Y Int deriving (Eq, Show)
 >>> newtype Z = Z Int deriving (Eq, Show)
 >>> x = HT (X 1) HNil
 >>> y = HT (Y 2) HNil
 >>> z = HT (Z 3) HNil
->>> xyz      = HT "xyz" (x :^: y :^: z :^: HNil)
->>> xyzUpper = HT "XYZ" (x :^: y :^: z :^: HNil)
->>> xzy      = HT "xzy" (x :^: z :^: y :^: HNil)
+>>> xyz      = HT "xyz" (x :>: y :>: (z >.))
+>>> xyzUpper = HT "XYZ" (x :>: y :>: (z >.))
+>>> xyzMeta  = HT "meta" (xyz :>: (xyz >.))
 >>> xyz
+>>> xyzMeta
 >>> xyz == xyzUpper
->>> xyz == xzy
-HT "xyz" (HT (X 1) HNil :^: (HT (Y 2) HNil :^: (HT (Z 3) HNil :^: HNil)))
+>>> xyz == xyzMeta
+HT "xyz" (HT (X 1) HNil :>: (HT (Y 2) HNil :>: (HT (Z 3) HNil >.)))
+HT "meta" (HT "xyz" (HT (X 1) HNil :>: (HT (Y 2) HNil :>: (HT (Z 3) HNil >.))) :>: (HT "xyz" (HT (X 1) HNil :>: (HT (Y 2) HNil :>: (HT (Z 3) HNil >.))) >.))
 False
 <BLANKLINE>
-<interactive>:17452:9-11: error:
-    • Couldn't match type ‘Z’ with ‘Y’
+<interactive>:5140:9-15: error:
+    • Couldn't match type ‘[Char]’ with ‘X’
       Expected type: HTree ('R [Char] '[ 'R X '[], 'R Y '[], 'R Z '[]])
-        Actual type: HTree ('R [Char] '[ 'R X '[], 'R Z '[], 'R Y '[]])
-    • In the second argument of ‘(==)’, namely ‘xzy’
-      In the expression: xyz == xzy
-      In an equation for ‘it’: it = xyz == xzy
+        Actual type: HTree
+                       ('R
+                          [Char]
+                          '[ 'R [Char] '[ 'R X '[], 'R Y '[], 'R Z '[]],
+                             'R [Char] '[ 'R X '[], 'R Y '[], 'R Z '[]]])
+    • In the second argument of ‘(==)’, namely ‘xyzMeta’
+      In the expression: xyz == xyzMeta
+      In an equation for ‘it’: it = xyz == xyzMeta
 
 -}
