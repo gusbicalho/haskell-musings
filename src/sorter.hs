1707fbc9a113fd8f7998d5e999d8d5641e67e2d9
Gustavo Bicalho <gusbicalho@gmail.com>
2019-06-02 19:12:26 +0000

    better sorter using interact

--- src/sorter.hs
+++ src/sorter.hs
@@ -1,19 +1,9 @@
 module Main where
 
-import System.IO
 import Data.List (sort)
 
-doall :: (Monad m) => (a -> m b) -> [a] -> m ()
-doall _ []     = return ()
-doall f (x:xs) = f x >> doall f xs
-
-pipeLines :: Handle -> Handle -> ([String] -> [String]) -> IO ()
-pipeLines ih oh f = do iStr <- hGetContents ih
-                       let output = f $ lines iStr
-                       doall (hPutStrLn oh) output
+pipeLinesStd :: ([String] -> [String]) -> IO ()
+pipeLinesStd f = interact (unlines . f . lines)
 
 main :: IO ()
-main = pipeLines stdin stdout processLines
-
-processLines :: [String] -> [String]
-processLines = sort
+main = pipeLinesStd sort
