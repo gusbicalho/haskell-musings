393d08a352f629ae48881b03564cf3eeac3433cc
Gustavo Bicalho <gusbicalho@gmail.com>
2020-08-16 21:24:17 +0000

    move things around

--- ChangeLog.md
+++
@@ -1,3 +0,0 @@
-# Changelog for haskell-musings
-
-## Unreleased changes
--- src/sorter.hs
+++
@@ -1,9 +0,0 @@
-module Main where
-
-import Data.List (sort)
-
-pipeLinesStd :: ([String] -> [String]) -> IO ()
-pipeLinesStd f = interact (unlines . f . lines)
-
-main :: IO ()
-main = pipeLinesStd sort
