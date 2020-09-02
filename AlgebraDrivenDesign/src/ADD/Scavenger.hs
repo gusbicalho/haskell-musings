074b8342e7f355ee777e9c198dfcf79b5c8d1550
Gustavo Bicalho <gusbicalho@gmail.com>
2020-09-02 18:13:24 +0000

    first point-of-interest algebra laws

--- AlgebraDrivenDesign/src/ADD/Scavenger.hs
+++ AlgebraDrivenDesign/src/ADD/Scavenger.hs
@@ -18,5 +18,36 @@ pointOfInterest _ _ _ _ = undefined
 photo :: Point -> Photo -> Input
 photo _ _ = undefined
 
+isPhoto :: Input -> Bool
+isPhoto _ = undefined
+
 within :: Point -> Point -> Distance -> Bool
 within _ _ _ = undefined
+
+-- Law "point of interest"
+-- forall c poi d r p pic is.
+--   within poi p d =>
+--     getRewards
+--       (pointOfInterest c poi d r)
+--       (photo p pic : is)
+--     = [r]
+
+-- Law "outside point of interest"
+-- forall c poi d r p pic is.
+--   not (within poi p d) =>
+--     getRewards
+--       (pointOfInterest c poi d r)
+--       (photo p pic : is)
+--     = getRewards
+--         (pointOfInterest c poi d r)
+--         is
+
+-- Law "unmatching point of interest"
+-- forall c poi d r p pic is.
+--   not (isPhoto i) =>
+--     getRewards
+--       (pointOfInterest c poi d r)
+--       (i : is)
+--     = getRewards
+--         (pointOfInterest c poi d r)
+--         is
