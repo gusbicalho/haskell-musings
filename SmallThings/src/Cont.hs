c8ec13f5478b223ad1d8c76b8846a1c263b3a597
Gustavo Bicalho <gusbicalho@gmail.com>
2020-09-04 18:06:13 +0000

    add Cont.hs

---
+++ SmallThings/src/Cont.hs
@@ -0,0 +1,37 @@
+{-# LANGUAGE InstanceSigs #-}
+{-# LANGUAGE RankNTypes #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+
+-- | Hand-crafted Cont (delimited continuation) monad
+module Cont where
+
+import Control.Monad (ap, liftM)
+
+newtype Cont r a = Cont {runCont :: (a -> r) -> r}
+
+cont :: ((a -> r) -> r) -> Cont r a
+cont = Cont
+
+instance Functor (Cont r) where
+  fmap = liftM
+
+instance Applicative (Cont r) where
+  pure a = Cont $ \k -> k a
+  (<*>) = ap
+
+instance Monad (Cont r) where
+  (>>=) :: forall a b. Cont r a -> (a -> Cont r b) -> Cont r b
+  (Cont continueProvidingA) >>= f = Cont $
+    \(completeExpectingB :: b -> r) ->
+      continueProvidingA $ \a ->
+        let (Cont continueProvidingB) = f a
+         in continueProvidingB completeExpectingB
+
+ex :: Cont [a] Integer
+ex = do
+  a <- pure 1
+  b <- cont (\k -> k 10 ++ k 20)
+  pure $ a + b
+
+-- >>> runCont ex show
+-- "1121"
