632f4a4df81205d7b226e4feefe341df2ea3c765
Gustavo Bicalho <gusbicalho@gmail.com>
2022-06-02 14:51:40 +0000

    implement extension that generalizes lambdas

---
+++ Bidirectional/src/BidirectionalWithPolyLam.hs
@@ -0,0 +1,158 @@
+{-# LANGUAGE BlockArguments #-}
+{-# LANGUAGE DerivingStrategies #-}
+{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE TypeFamilies #-}
+
+-- | Extended to infer polymorphic types for lambdas
+module BidirectionalWithPolyLam where
+
+import Bidirectional.Context qualified as Ctx
+import Bidirectional.ContextState qualified as CtxState
+import Bidirectional.FreshVar qualified as FreshVar
+import Bidirectional.Language
+import Bidirectional.ReportTypeErrors
+import Bidirectional.Subtyping (isSubtypeOf)
+import Control.Monad.Trans.Except (Except)
+import Control.Monad.Trans.Except qualified as Except
+import qualified Data.Maybe as Maybe
+import Data.Function ((&))
+
+--------------------------------------------------------------------------------
+-- Effects stack
+
+type TC = CtxState.CtxStateT (FreshVar.FreshT (Except String))
+
+runTC :: TC a -> Either String a
+runTC = Except.runExcept . FreshVar.runFreshT . CtxState.evalCtx Ctx.emptyCtx
+
+--------------------------------------------------------------------------------
+-- Type checking
+
+typeComplete :: Expr -> Either String Tipe
+typeComplete expr = runTC do
+  tipe <- typeSynth expr
+  finalCtx <- CtxState.getCtx
+  pure $ Ctx.substCtxInType finalCtx tipe
+
+typeSynth :: Expr -> TC Tipe
+typeSynth = goSynth
+ where
+  goSynth = \case
+    -- Var
+    EVar varName ->
+      CtxState.varIsBoundToATerm varName
+    -- Anno
+    EAnno expr tipe -> do
+      CtxState.withCtx \ctx -> Ctx.isWellFormedType ctx tipe
+      typeCheck expr tipe
+      pure tipe
+    -- ->E
+    expr@(EApply callee argument) -> do
+      calleeType <- do
+        calleeType <- goSynth callee
+        ctx <- CtxState.getCtx
+        pure $ Ctx.substCtxInType ctx calleeType
+      typeApply expr calleeType argument
+    -- 1I⇒
+    EUnit -> pure TUnit
+    -- ->I⇒
+    ELam argName body -> do
+      let argVar = NamedVar argName
+      alpha <- FreshVar.freshVar "->I⇒_arg"
+      let argType = TVar alpha
+      beta <- FreshVar.freshVar "->I⇒_ret"
+      let retType = TVar beta
+      -- The extension starts here
+      -- In the original version, we create two existentials and then leave them
+      -- in the context. These existentials may be solved when checking the body
+      -- of the lambda, or they may be left unsolved, and may be solved later
+      -- when checking the use sites for the lambda.
+      -- In this extension, any existentials left unsolved after checking the
+      -- body of the lambda are transformed into universals, by wrapping the
+      -- inferred TFunction type into many TForalls.
+      CtxState.markExistential alpha
+      CtxState.bindOpenExistential alpha
+      CtxState.bindOpenExistential beta
+      CtxState.bindTermVar argVar argType
+      typeCheck body retType
+      funType <- Ctx.substCtxInType <$> CtxState.getCtx <*> pure (TFunction argType retType)
+      (_, droppedEntries) <- CtxState.dropEntriesUntilMarkerOf alpha
+      let unsolvedExistentials = droppedEntries & Maybe.mapMaybe \case
+            Right (v, Ctx.IsExistential Nothing) -> Just v
+            _ -> Nothing
+      let generalizedFunType = foldr TForall funType unsolvedExistentials
+      pure generalizedFunType
+
+typeCheck :: Expr -> Tipe -> TC ()
+typeCheck = goCheck
+ where
+  -- ForallI
+  goCheck expr (TForall forallVar univType) = do
+    CtxState.bindUniversal forallVar
+    goCheck expr univType
+    CtxState.dropEntriesUntilBinding_ forallVar
+  -- ->I
+  goCheck (ELam argName body) (TFunction argType retType) = do
+    let argVar = NamedVar argName
+    CtxState.bindTermVar argVar argType
+    goCheck body retType
+    CtxState.dropEntriesUntilBinding_ argVar
+  -- 1I
+  goCheck EUnit TUnit = pure ()
+  -- Sub
+  goCheck expr expectedType = do
+    foundType <- typeSynth expr
+    CtxState.onCtx (isSubtypeOf foundType expectedType)
+      `catchTypeError` \err ->
+        typeError
+          [ err
+          , "when typechecking expression"
+          , ind [show expr]
+          ]
+
+typeApply :: Expr -> Tipe -> Expr -> TC Tipe
+typeApply overallApplyExpr = goApply
+ where
+  -- ForallApp
+  goApply (TForall forallVar univType) argument = do
+    CtxState.bindOpenExistential forallVar
+    goApply univType argument
+  -- ->App
+  goApply (TFunction argType retType) argument = do
+    typeCheck argument argType
+    pure retType
+  -- ExistApp
+  goApply tipe@(TVar alpha) argument = do
+    Ctx.lookupBinding alpha <$> CtxState.getCtx >>= \case
+      Just (Ctx.IsExistential Nothing) -> do
+        argVar <- FreshVar.freshVar "ExistApp_arg"
+        retVar <- FreshVar.freshVar "ExistApp_ret"
+        CtxState.articulateExistential
+          alpha
+          [retVar, argVar]
+          (Ctx.MonoFunction (Ctx.MonoVar argVar) (Ctx.MonoVar retVar))
+        typeCheck argument (TVar argVar)
+        pure (TVar retVar)
+      _ -> badApplyError tipe
+  goApply tipe _ = badApplyError tipe
+  badApplyError tipe =
+    typeError $
+      [ "Expected polymorphic or function type, but got"
+      , ind [show tipe]
+      , "in application expression"
+      , ind [show overallApplyExpr]
+      ]
+
+substType :: Var -> Tipe -> Tipe -> Tipe
+substType replacedVar replacement = go
+ where
+  go TUnit = TUnit
+  go (TFunction argType retType) = TFunction (go argType) (go retType)
+  go t@(TVar var)
+    | var == replacedVar = replacement
+    | otherwise = t
+  go t@(TForall forallVar boundType)
+    -- shadowing
+    | forallVar == replacedVar = t
+    -- no shadowing
+    | otherwise = TForall forallVar (go boundType)
--- Bidirectional/src/Examples.hs
+++ Bidirectional/src/Examples.hs
@@ -2,6 +2,7 @@ module Examples where
 import Bidirectional.Language
 import Bidirectional qualified
 import BidirectionalWithImplicitCtx qualified
+import BidirectionalWithPolyLam qualified
 
 test_id :: Expr
 test_id = ELam "x" (EVar (NamedVar "x"))
@@ -21,16 +22,36 @@ test_constUnit = EApply test_const EUnit
 test_always_unit :: Expr
 test_always_unit = ELam "x" EUnit
 
+test_const_higher_rank :: Expr
+test_const_higher_rank =
+  EAnno
+    test_const
+    (TForall
+      (NamedVar "T")
+      (TFunction
+        (TVar (NamedVar "T"))
+        (TForall
+          (NamedVar "U")
+          (TFunction
+            (TVar (NamedVar "U"))
+            (TVar (NamedVar "T"))))))
+
+test_const_higher_rank_unit :: Expr
+test_const_higher_rank_unit = EApply test_const_higher_rank EUnit
+
 -- Helper
 
 expectRight :: Either String c -> c
 expectRight = either error id
 
+testBidirectional :: Expr -> Tipe
+testBidirectional = expectRight . Bidirectional.typeComplete
+
 testBidirectionalWithImplicitCtx :: Expr -> Tipe
 testBidirectionalWithImplicitCtx = expectRight . BidirectionalWithImplicitCtx.typeComplete
 
-testBidirectional :: Expr -> Tipe
-testBidirectional = expectRight . Bidirectional.typeComplete
+testBidirectionalWithPolyLam :: Expr -> Tipe
+testBidirectionalWithPolyLam = expectRight . BidirectionalWithPolyLam.typeComplete
 
 {-
 
@@ -55,6 +76,12 @@ TFunction (TVar (FreshVar "->I\8658_arg" 0)) TUnit
 >>> testBidirectional $ test_constUnit
 TFunction (TVar (FreshVar "InstRArr_arg" 4)) TUnit
 
+>>> testBidirectional $ test_const_higher_rank
+TForall (NamedVar "T") (TFunction (TVar (NamedVar "T")) (TForall (NamedVar "U") (TFunction (TVar (NamedVar "U")) (TVar (NamedVar "T")))))
+
+>>> testBidirectional $ test_const_higher_rank_unit
+TForall (NamedVar "U") (TFunction (TVar (NamedVar "U")) TUnit)
+
 --------------------------------------------------------------------------------
 BidirectionalWithImplicitCtx
 
@@ -75,5 +102,38 @@ TFunction (TVar (FreshVar "->I\8658_arg" 0)) TUnit
 
 >>> testBidirectionalWithImplicitCtx $ test_constUnit
 TFunction (TVar (FreshVar "InstRArr_arg" 4)) TUnit
+
+>>> testBidirectionalWithImplicitCtx $ test_const_higher_rank
+TForall (NamedVar "T") (TFunction (TVar (NamedVar "T")) (TForall (NamedVar "U") (TFunction (TVar (NamedVar "U")) (TVar (NamedVar "T")))))
+
+>>> testBidirectionalWithImplicitCtx $ test_const_higher_rank_unit
+TForall (NamedVar "U") (TFunction (TVar (NamedVar "U")) TUnit)
+
+--------------------------------------------------------------------------------
+BidirectionalWithPolyLam
+
+>>> testBidirectionalWithPolyLam $ test_id
+TForall (FreshVar "->I\8658_arg" 0) (TFunction (TVar (FreshVar "->I\8658_arg" 0)) (TVar (FreshVar "->I\8658_arg" 0)))
+
+>>> testBidirectionalWithPolyLam $ test_call_id
+TUnit
+
+>>> testBidirectionalWithPolyLam $ test_anno_id
+TForall (NamedVar "T") (TFunction (TVar (NamedVar "T")) (TVar (NamedVar "T")))
+
+>>> testBidirectionalWithPolyLam $ test_const
+TForall (FreshVar "->I\8658_arg" 0) (TForall (FreshVar "InstRArr_arg" 4) (TFunction (TVar (FreshVar "->I\8658_arg" 0)) (TFunction (TVar (FreshVar "InstRArr_arg" 4)) (TVar (FreshVar "->I\8658_arg" 0)))))
+
+>>> testBidirectionalWithPolyLam $ test_always_unit
+TForall (FreshVar "->I\8658_arg" 0) (TFunction (TVar (FreshVar "->I\8658_arg" 0)) TUnit)
+
+>>> testBidirectionalWithPolyLam $ test_constUnit
+TFunction (TVar (FreshVar "InstRArr_arg" 4)) TUnit
+
+>>> testBidirectionalWithPolyLam $ test_const_higher_rank
+TForall (NamedVar "T") (TFunction (TVar (NamedVar "T")) (TForall (NamedVar "U") (TFunction (TVar (NamedVar "U")) (TVar (NamedVar "T")))))
+
+>>> testBidirectionalWithPolyLam $ test_const_higher_rank_unit
+TForall (NamedVar "U") (TFunction (TVar (NamedVar "U")) TUnit)
 
 -}
