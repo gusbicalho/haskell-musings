bb2c7f757d2588b82fd49e4cb270b4158e8f7740
Gustavo Bicalho <gusbicalho@gmail.com>
2024-05-05 20:47:05 +0100

    hanoi second try did not work

--- SmallThings/src/Hanoi.hs
+++ SmallThings/src/Hanoi.hs
@@ -1,5 +1,7 @@
 {-# LANGUAGE BangPatterns #-}
+{-# LANGUAGE BlockArguments #-}
 {-# LANGUAGE DataKinds #-}
+{-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE FlexibleInstances #-}
 {-# LANGUAGE GADTs #-}
 {-# LANGUAGE MultiParamTypeClasses #-}
@@ -12,7 +14,6 @@
 {-# LANGUAGE TypeOperators #-}
 {-# LANGUAGE UndecidableInstances #-}
 {-# LANGUAGE NoStarIsType #-}
-{-# LANGUAGE BlockArguments #-}
 {-# OPTIONS_GHC -Wno-unused-imports #-}
 {-# OPTIONS_GHC -Wno-unused-top-binds #-}
 
@@ -61,6 +62,11 @@ data Tower id ns where
   First :: SNat n -> Tower id '[] -> Tower id '[n]
   More :: SNat n -> n < top -> Tower id (top : discs) -> Tower id (n : top : discs)
 
+popTower :: (discs ~ (a : b : more)) => Tower id discs -> (Tower id (Popped discs), Tower id '[(Last discs)])
+popTower (More a _ltA (First b empty)) = (First a empty, First b empty)
+popTower (More a ltA (More b ltB more)) = case popTower (More b ltB more) of
+  (more, last) -> (More a ltA more, last)
+
 data Hanoi a as b bs c cs where
   H :: Tower a as -> Tower b bs -> Tower c cs -> Hanoi a as b bs c cs
 
@@ -71,18 +77,34 @@ data discs `CanReceive` top where
 
 type ProveForAll :: forall k. (k -> Type) -> [k] -> Type
 data ProveForAll prover elems where
+  Vacuous :: ProveForAll prover '[]
   ProveForOne :: prover one -> ProveForAll prover '[one]
   ProveForMore :: prover one -> ProveForAll prover (two : more) -> ProveForAll prover (one : two : more)
 
 class AllForOne prover elems elem where
   allForOne :: ProveForAll prover elems -> prover elem
 
-instance AllForOne prover (disc : more) disc where
+instance {-# OVERLAPPING #-} AllForOne prover (disc : more) disc where
   allForOne (ProveForOne proof) = proof
   allForOne (ProveForMore proof _) = proof
 
-instance (AllForOne prover more disc, more ~ (two : many)) => AllForOne prover (any : more) disc where
+instance {-# OVERLAPPABLE #-} (AllForOne prover more disc, more ~ (two : many)) => AllForOne prover (any : more) disc where
   allForOne (ProveForMore _ more) = allForOne more
+
+type Popped :: forall k. [k] -> [k]
+type family Popped xs where
+  Popped '[x] = '[]
+  Popped (x : more) = x : Popped more
+
+type Last :: forall k. [k] -> k
+type family Last xs where
+  Last '[x] = x
+  Last (_ : more) = Last more
+
+popProof :: (elems ~ (a : b : more)) => ProveForAll prover elems -> (ProveForAll prover (Popped elems), prover (Last elems))
+popProof (ProveForMore a (ProveForOne b)) = (ProveForOne a, b)
+popProof (ProveForMore a (ProveForMore b more)) = case popProof (ProveForMore b more) of
+  (more, last) -> (ProveForMore a more, last)
 
 -- instance AllForOneAllForOne (CanReceive discs) '[disc] disc where
 --   allForOne (ProveForOne proof) = proof
@@ -114,20 +136,48 @@ two (H ta tb tc) bCanReceive cCanReceive = runIdentity $ do
   (tb, tc, _) <- pure $ moveTop bCanReceiveFromC tb tc
   pure $ H ta tb tc
 
+emptyCanReceiveAll :: (xs ~ (_a : _more)) => Tower x xs -> ProveForAll (CanReceive '[]) xs
+emptyCanReceiveAll (First _x _empty) = ProveForOne IsEmpty
+emptyCanReceiveAll (More _ _ more) = ProveForMore IsEmpty (emptyCanReceiveAll more)
+
 -- solve ::
 --   forall a as b bs c cs.
 --   ProveForAll (CanReceive bs) as ->
 --   ProveForAll (CanReceive cs) as ->
+--   ProveForAll (CanReceive bs) cs ->
 --   Hanoi a as b bs c cs ->
---   Hanoi a '[] b bs c (as ++ cs)
--- solve bCanReceive cCanReceive (H ta tb tc) =
---   case (bCanReceive, cCanReceive) of
---     (ProveForOne _bCanReceive, ProveForOne cCanReceive) -> runIdentity do
---       First top emptyA <- pure ta
---       pure $ H emptyA tb (putDisc cCanReceive top tc)
---     (ProveForMore bCanReceiveTop bCanReceiveMore, ProveForMore cCanReceiveTop cCanReceiveMore) -> runIdentity do
+--   Either (Hanoi a '[] b bs c (as ++ cs)) (Hanoi a '[] b (as ++ bs) c cs)
+-- solve bCanReceiveA cCanReceiveA bCanReceiveC (H ta tb tc) = case ta of
+--   Empty _ -> Right $ H ta tb tc
+--   First top empty -> Right $ H empty (putDisc (allForOne bCanReceiveA) top tb) tc
+--   More _ _ _ -> runIdentity do
+--     (top, last) <- pure $ popTower ta
+--     (bCanReceiveTop, bCanReceiveLast) <- pure $ popProof bCanReceiveA
+--     (cCanReceiveTop, cCanReceiveLast) <- pure $ popProof cCanReceiveA
+--     case solve bCanReceiveTop cCanReceiveTop bCanReceiveC (H top tb tc) of
+--       Right (H top tb tc) -> case last of
+--         First lastDisc emptyA -> case putDisc cCanReceiveLast lastDisc tc of
+--           !tc -> runIdentity do
+--             let h = (H tb tc emptyA)
+--             _
+--       Left (H top tb tc) -> runIdentity do
+--         let First lastDisc emptyA = last
+--         _
 
---       _
+-- H tc tb _ <- pure $ solve bCanReceiveC (emptyCanReceiveAll tc) Vacuous (H tc tb (Empty ()))
+-- tb <- pure $ putDisc (bCanReceiveLast) last tb
+-- _
+
+-- case (bCanReceive, cCanReceive) of
+--   (ProveForOne _bCanReceive, ProveForOne cCanReceive) -> case ta of
+--     First top emptyA -> H emptyA tb (putDisc cCanReceive top tc)
+--     More top _lt moreA -> H moreA tb (putDisc cCanReceive top tc)
+--   (ProveForMore _ _, ProveForMore _ _) -> runIdentity do
+--     (bCanReceiveTop, bCanReceiveLast) <- pure $ popProof bCanReceive
+--     (cCanReceiveTop, cCanReceiveLast) <- pure $ popProof cCanReceive
+--     (top, last) <- pure $ popTower ta
+--     h2 <- pure $ solve bCanReceiveTop cCanReceiveTop (H top tb tc)
+--     _
 
 -- case ta of
 --   Empty _ -> (H ta tb tc, ())
